e0ba6f9b06bf74e23f9f1c80c6f758463fe64216
Andréas Idh <andreas.idh@realsprint.com>
2018-11-08 09:55:50 +0100

    added proxy

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    fetch('/api/', {
+      headers: {
+         authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbmRyZWFzIiwicm9sZXMiOiJ1c2VyIiwiaWF0IjoxNTQxNjMwNDUyfQ.2q77hz2-H0AXvSmGjiu1Q6OffMEM2aJcrgVzUk9WRHE'
+       }
+    })
+    .then( response => response.json())
+    .then( users => {
+      if(users.status !== 500) {
+        this.setState({ users })
+      }
+    });
+  }
+
   render() {
+    const users = this.state.users.map( user => (<div key={user.id}>{user.username}</div>));
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {users}
         </header>
       </div>
     );
